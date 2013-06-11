e51959aedb88857e26deb3de255e7e029cafa78d
Your Name Here <bogdan98nitu@gmail.com>
2013-06-11 22:28:56 +0300

    GUI Package

---
+++ bi/bi_gui/GuiHandlerC.java
@@ -0,0 +1,43 @@
+package bi.bi_gui;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
+import bi.bi_Blocks.ModBlocks;
+import cpw.mods.fml.common.network.IGuiHandler;
+
+public class GuiHandlerC implements IGuiHandler
+{
+
+	@Override
+	public Object getServerGuiElement(int id, EntityPlayer player, World world, int x, int y, int z)
+	{
+
+		TileEntity tile_entity = world.getBlockTileEntity(x, y, z);
+
+		switch(id)
+		{
+
+		case 1: return id == 1 && world.getBlockId(x, y, z) == ModBlocks.Crafter.blockID ? new ContainerC(player.inventory, world, x, y, z) : null;
+
+		}
+		return null;
+	}
+
+	@Override
+	public Object getClientGuiElement(int id, EntityPlayer player, World world, int x, int y, int z)
+	{
+
+		TileEntity tile_entity = world.getBlockTileEntity(x, y, z);
+
+		switch(id)
+		{
+
+		case 1: return id == 1 && world.getBlockId(x, y, z) == ModBlocks.Crafter.blockID ? new GuiC(player.inventory, world, x, y, z) : null;
+
+		}
+
+		return null;
+
+	}
+}
